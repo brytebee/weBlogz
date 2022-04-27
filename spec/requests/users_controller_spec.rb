28e3332ca952c7e84fd35d160efcb52c7297c1d9
brytebee <brytebee@gmail.com>
2022-04-27 18:31:46 +0100

    :test_tube: Add test
    Added request test to check if the users pages content match

--- spec/requests/users_controller_spec.rb
+++ spec/requests/users_controller_spec.rb
@@ -5,9 +5,19 @@ RSpec.describe "Users controller", :type => :request do
     get "/users"
     expect(response).to render_template(:index)
   end
+  
+  it "displays the users index page" do
+    get "/users"
+    expect(response.body).to include('This is begininng to make sense oooo')
+  end
 
   it "displays the users show page" do
     get "/users/20"
     expect(response).to render_template(:show)
+  end
+  
+  it "displays the users show page" do
+    get "/users/20"
+    expect(response.body).to include('This is the sample user show page')
   end
 end
