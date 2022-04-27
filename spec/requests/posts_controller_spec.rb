be0878cb4bd04b292a6c4ff8771fd2a5735c0470
brytebee <brytebee@gmail.com>
2022-04-27 17:49:00 +0100

    :test_tube: Add initial test
    Added test

---
+++ spec/requests/posts_controller_spec.rb
@@ -0,0 +1,24 @@
+require "rails_helper"
+
+RSpec.describe "Posts controller", :type => :request do
+
+  it "creates a Widget and redirects to the Widget's page" do
+    # get "/widgets/new"
+    # expect(response).to render_template(:new)
+    get "/users/2/posts/1"
+    expect(response).to render_template(:show)
+
+    # post "/widgets", :widget => {:name => "My Widget"}
+
+    # expect(response).to redirect_to(assigns(:widget))
+    # follow_redirect!
+
+    # expect(response).to render_template(:show)
+    # expect(response.body).to include("Widget was successfully created.")
+  end
+
+  # it "does not render a different template" do
+  #   get "/widgets/new"
+  #   expect(response).to_not render_template(:show)
+  # end
+end
