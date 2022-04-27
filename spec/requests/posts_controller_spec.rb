5f1aef9ed550411db6568e39afaa664f9fbee591
brytebee <brytebee@gmail.com>
2022-04-27 18:37:02 +0100

    :test_tube: Add test
    Added request test to check match to the posts show page ccontent

--- spec/requests/posts_controller_spec.rb
+++ spec/requests/posts_controller_spec.rb
@@ -7,7 +7,10 @@ RSpec.describe "Posts controller", :type => :request do
     expect(response).to render_template(:show)
   end
 
-  # it ""
+  it "displays the page content on the posts show page" do
+    get "/users/2/posts/1"
+    expect(response.body).to include('This is the sample posts show page')
+  end
 
   it "displays the posts index page" do
     get "/users/2/posts"
