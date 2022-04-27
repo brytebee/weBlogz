2c3662f1d0d7cd73ed78f6e4c3003fd9b5b60946
brytebee <brytebee@gmail.com>
2022-04-27 18:13:19 +0100

    :test_tube: Add test
    Added request test to display users index page

--- spec/requests/users_controller_-spec.rb
+++ spec/requests/users_controller_-spec.rb
@@ -1,4 +1,8 @@
 require "rails_helper"
 
 RSpec.describe "Users controller", :type => :request do
+  it "displays the users index page" do
+    get "/users"
+    expect(response).to render_template(:index)
+  end
 end
