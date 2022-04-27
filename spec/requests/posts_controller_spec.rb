320518e405da74ab84180924150b37c44a57cd59
brytebee <brytebee@gmail.com>
2022-04-27 18:40:41 +0100

    :rotating_light: Fix linters
    Fixed

--- spec/requests/posts_controller_spec.rb
+++ spec/requests/posts_controller_spec.rb
@@ -1,24 +1,23 @@
-require "rails_helper"
+require 'rails_helper'
 
-RSpec.describe "Posts controller", :type => :request do
-
-  it "displays the posts show page" do
-    get "/users/2/posts/1"
+RSpec.describe 'Posts controller', type: :request do
+  it 'displays the posts show page' do
+    get '/users/2/posts/1'
     expect(response).to render_template(:show)
   end
 
-  it "displays the page content on the posts show page" do
-    get "/users/2/posts/1"
+  it 'displays the page content on the posts show page' do
+    get '/users/2/posts/1'
     expect(response.body).to include('This is the sample posts show page')
   end
 
-  it "displays the posts index page" do
-    get "/users/2/posts"
+  it 'displays the posts index page' do
+    get '/users/2/posts'
     expect(response).to render_template(:index)
   end
 
-  it "displays the posts index page content" do
-    get "/users/2/posts"
+  it 'displays the posts index page content' do
+    get '/users/2/posts'
     expect(response.body).to include('Post#Index')
   end
 end
--- spec/requests/users_controller_spec.rb
+++ spec/requests/users_controller_spec.rb
@@ -1,23 +1,23 @@
-require "rails_helper"
+require 'rails_helper'
 
-RSpec.describe "Users controller", :type => :request do
-  it "displays the users index page" do
-    get "/users"
+RSpec.describe 'Users controller', type: :request do
+  it 'displays the users index page' do
+    get '/users'
     expect(response).to render_template(:index)
   end
-  
-  it "displays the users index page content" do
-    get "/users"
+
+  it 'displays the users index page content' do
+    get '/users'
     expect(response.body).to include('This is begininng to make sense oooo')
   end
 
-  it "displays the users show page" do
-    get "/users/20"
+  it 'displays the users show page' do
+    get '/users/20'
     expect(response).to render_template(:show)
   end
-  
-  it "displays the users show page content" do
-    get "/users/20"
+
+  it 'displays the users show page content' do
+    get '/users/20'
     expect(response.body).to include('This is the sample user show page')
   end
 end
