81935b6dabd5ad84fcb3303cd9ff17027534819c
brytebee <brytebee@gmail.com>
2022-04-27 10:04:01 +0100

    :fire: Remove user, post and new methods

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,5 @@
 class PostsController < ApplicationController
   def index; end
 
-  def posts; end
-
   def show; end
-
-  def new; end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,5 @@
 class UsersController < ApplicationController
   def index; end
 
-  def users; end
-
   def show; end
-
-  def new; end
 end
