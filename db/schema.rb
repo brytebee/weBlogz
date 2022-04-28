03243de9ab00ff04a1ab741bc30179d5f81ba232
brytebee <brytebee@gmail.com>
2022-04-28 19:46:46 +0100

    :white_check_mark: Update schema.rb
    Removed post_id from comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_28_183220) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_28_184301) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
-    t.integer "post_id"
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
