21061c0e050bde1502754a3e8930550eb087dee2
brytebee <brytebee@gmail.com>
2022-04-28 20:30:13 +0100

    :fire: Remove migration
    Removed AddAuthorToComments

--- db/migrate/20220428190829_add_author_ref_to_comments.rb
+++
@@ -1,7 +0,0 @@
-class AddAuthorRefToComments < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :comments, :author, null: false, foreign_key: true
-    add_column :comments, :author, :integer
-    add_index :comments, :author
-  end
-end
