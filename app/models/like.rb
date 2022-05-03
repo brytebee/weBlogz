19cf6bedc34ff3a56fd9d132c7035153446233ea
brytebee <brytebee@gmail.com>
2022-05-03 18:24:44 +0100

    :white_check_mark: Update Like model
    Added update_post_counter

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,8 @@
 class Like < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   belongs_to :post
+
+  def update_post_counter
+    post.increment!(:posts_counter)
+  end
 end
