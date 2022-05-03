a9ed676cde7e0a448601c8fc1543eef4a90adf9e
brytebee <brytebee@gmail.com>
2022-05-03 18:48:08 +0100

    :white_check_mark: Update Post model
    Add recent_comment method

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,10 @@ class Post < ApplicationRecord
   has_many :likes
   has_many :comments
 
+  def recent_comment
+    comments.where(author_id.params[:id]).order(created_at: :desc).limit(5)
+  end
+
   private
 
   def increment_posts_counter
