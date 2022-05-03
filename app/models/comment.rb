f8135f9417a5307a2e4a6ddd20752db610c43101
brytebee <brytebee@gmail.com>
2022-05-03 18:34:24 +0100

    :white_check_mark: Update Comment model
    Changed update_comment_counter access to private

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,8 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   belongs_to :post
 
+  private
+
   def update_comment_counter
     post.increment!(:posts_counter)
   end
