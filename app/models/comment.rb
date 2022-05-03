40e0c2acd7e3967511a8c35ca509fa194c485ad3
brytebee <brytebee@gmail.com>
2022-05-03 18:30:34 +0100

    :white_check_mark: Update Comment model
    Add update_comment_counter

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   belongs_to :post
+
+  def update_comment_counter
+    post.increment!(:posts_counter)
+  end
 end
