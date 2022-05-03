825f15d052aa9e8e3b75e61ed39f5278e54940aa
brytebee <brytebee@gmail.com>
2022-05-03 19:15:04 +0100

    :fire: Update likes counter
    Updated method name to update likes counter from update post counter

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,10 +1,10 @@
 class Like < ApplicationRecord
-  after_save :update_post_counter
+  after_save :update_likes_counter
 
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   belongs_to :post
 
-  def update_post_counter
+  def update_likes_counter
     post.increment!(:likes_counter)
   end
 end
