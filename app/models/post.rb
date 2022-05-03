cea906c61aae4132cc54b1470ddc741506e1750b
brytebee <brytebee@gmail.com>
2022-05-03 18:45:03 +0100

    :white_check_mark: Update Post model
    Add increment_posts_counter method

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_many :likes
   has_many :comments
+
+  def increment_posts_counter
+    author.increment!(:posts_counter)
+  end
 end
