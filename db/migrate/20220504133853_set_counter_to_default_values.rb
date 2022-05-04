e94d5449a1a56d7d68e0f70a0d6bd125aa0d80d6
brytebee <brytebee@gmail.com>
2022-05-04 14:47:03 +0100

    :fire: Updatemigration
    Uodated post_counter to posts_counter

--- db/migrate/20220504133853_set_counter_to_default_values.rb
+++ db/migrate/20220504133853_set_counter_to_default_values.rb
@@ -1,6 +1,6 @@
 class SetCounterToDefaultValues < ActiveRecord::Migration[7.0]
   def change
-    change_column_default :users, :post_counter, 0
+    change_column_default :users, :posts_counter, 0
     change_column_default :posts, :comments_counter, 0
     change_column_default :posts, :likes_counter, 0
   end
