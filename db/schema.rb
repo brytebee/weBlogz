fc6d92586f9b75602ad72f6d2eccbb8c28004d68
brytebee <brytebee@gmail.com>
2022-05-04 10:41:06 +0100

    :fire: Clean up
    Renamed post_counter column to posts_counter in users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_03_123923) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_04_093927) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,6 +40,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_03_123923) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "author_id", null: false
+    t.string "title"
     t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
@@ -47,9 +48,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_03_123923) do
     t.string "name"
     t.string "photo"
     t.text "bio"
-    t.integer "post_counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "posts_counter"
   end
 
   add_foreign_key "comments", "posts"
