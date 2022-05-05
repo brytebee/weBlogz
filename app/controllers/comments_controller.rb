7665fa4d2d2a53597019a7657f8bc3caa129e5cc
brytebee <brytebee@gmail.com>
2022-05-05 22:53:34 +0100

    :white_check_mark: Update CommentsController
    Add create method

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,4 +2,16 @@ class CommentsController < ApplicationController
   def new
     @comment = Comment.new
   end
+
+	def create
+    @comment = @current_user.comments.new(params)
+    @comment.post_id = params[:post_id]
+
+    if @comment.save
+      redirect_to post_path(@comment.post)
+    else
+      @article = Post.find(params[:post_id])
+      redirect_to post_path(@post)
+    end
+  end
 end
