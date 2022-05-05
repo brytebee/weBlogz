9bf20d998b2b0c7fdc620afc966006300a6f174d
brytebee <brytebee@gmail.com>
2022-05-05 23:24:51 +0100

    :white_check_mark: Update routes.rb
    Added comments resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show new create]
+    resources :posts, only: %i[index show new create] do
+      resources :comments, only: %i[new create]
+    end
   end
   
   root "users#index"
