c323b6bad96abacab9a6f31969a823f1f97429ca
brytebee <brytebee@gmail.com>
2022-05-06 14:13:30 +0100

    :white_check_mark: Update LikesController
    Updated LikesController to be functional

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -5,11 +5,12 @@ class LikesController < ApplicationController
 
   def create
     @post = Post.find(params[:post_id])
-    @like = Like.new(post_id: @post.id)
+    @user = current_user
+    @like = Like.new(author_id: @user.id, post_id: @post.id)
 
     respond_to do |format|
       if @like.save
-        format.html { redirect_to @post }
+        format.html { redirect_to user_post_path(user_id: @post.author_id, id: @post.id) }
       else
         format.html { redirect_to @post }
       end
