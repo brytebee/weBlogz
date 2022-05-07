f0e7eca5bf6540f655c922d1bb7e3cc3773d13ff
brytebee <brytebee@gmail.com>
2022-05-07 13:54:46 +0100

    :fire: Clean up
    Cleaned up!

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -24,7 +24,6 @@ RSpec.configure do |config|
     config.after(:each)  { Bullet.end_request }
   end
 
-
   config.expect_with :rspec do |expectations|
     # This option will default to `true` in RSpec 4. It makes the `description`
     # and `failure_message` of custom matchers include text for helper methods
