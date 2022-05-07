9f061968a683dee8a9d35f83056496424aa387e2
brytebee <brytebee@gmail.com>
2022-05-07 15:34:12 +0100

    :white_check_mark: Added comment success notice
    Added comment success notice

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -11,7 +11,7 @@ class CommentsController < ApplicationController
 
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to user_post_path(user_id: @post.author_id, id: @post.id) }
+        format.html { redirect_to user_post_path(user_id: @post.author_id, id: @post.id), notice: "Comment created successfully!" }
       else
         format.html { render :new, alert: 'An error has occurred while creating the comment!', class: 'alert-danger' }
       end
