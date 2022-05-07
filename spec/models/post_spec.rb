6186dff05b821adf92eda6c94668f083e287ed10
brytebee <brytebee@gmail.com>
2022-05-07 15:59:19 +0100

    :test_tube: Update post_spec.rb

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,2 +1,15 @@
+require 'rails_helper'
 
+RSpec.describe Post, type: :model do
+  subject { Post.new(title: "Good post", text: "this is my post") }
 
+  it "Title must not be blank" do
+    subject.title = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "Title must not exceed 250 characters" do
+    subject.title.length <= 300
+    expect(subject).to_not be_valid
+  end
+end
