fce1a11eaeae2462d509e0b5e1a1cb291ff941d2
brytebee <brytebee@gmail.com>
2022-05-07 15:52:06 +0100

    :white_check_mark: Add post validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,13 @@ class Post < ApplicationRecord
   has_many :likes
   has_many :comments
 
+  validates :title, presence: true
+  validates_associated :author
+  validates_presence_of :author_id
+  validates :title, length: { maximum: 250 }
+  validates :comments_counter, comparison: { greater_than_or_equal_to: 0}
+  validates :likes_counter, comparison: { greater_than_or_equal_to: 0}
+
   def recent_comments
     comments.order(created_at: :desc).limit(5)
   end
