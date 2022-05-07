5b42778d1a775168b10056813695788f0877598d
brytebee <brytebee@gmail.com>
2022-05-07 16:01:01 +0100

    :test_tube: Update user_spec.rb
    Added more tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,4 +7,19 @@ RSpec.describe User, type: :model do
     subject.name = nil
     expect(subject).to_not be_valid
   end
+
+  it "Name must not be blank" do
+    subject.name = "Anny"
+    expect(subject).to be_valid
+  end
+
+  it "Post counter must be an integer" do
+    subject.post_counter = "Bright"
+    expect(subject).to_not be_valid
+  end
+
+  it "Post counter must be an integer" do
+    subject.post_counter = 1
+    expect(subject).to be_valid
+  end
 end
