c590175d29324417e5d2624e9ab92dfdfd70c995
brytebee <brytebee@gmail.com>
2022-05-10 22:58:19 +0100

    :white_check_mark: Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, only: %i[index show] do
-    resources :posts, only: %i[index show new create] do
+  resources :users, only: %i[index show destroy] do
+    resources :posts, only: %i[index show new create destroy] do
       resources :comments, only: %i[new create]
       resources :likes, only: %i[new create]
     end
