4a96e6547f4eccc46f6f55bd873660649647b058
brytebee <brytebee@gmail.com>
2022-05-10 22:39:44 +0100

    :white_check_mark: Update schema

---
+++ db/migrate/20220510195304_update_foreign_key.rb
@@ -0,0 +1,14 @@
+class UpdateForeignKey < ActiveRecord::Migration[7.0]
+  def change
+     remove_foreign_key :posts, :users
+     add_foreign_key :posts, :users, on_delete: :cascade, class_name: "User", column: "author_id"
+     remove_foreign_key :likes, :users
+     add_foreign_key :likes, :users, on_delete: :cascade, class_name: "User", column: "author_id"
+     remove_foreign_key :likes, :posts
+     add_foreign_key :likes, :posts, on_delete: :cascade
+     remove_foreign_key :comments, :users
+     add_foreign_key :comments, :users, on_delete: :cascade, class_name: "User", column: "author_id"
+     remove_foreign_key :comments, :posts
+     add_foreign_key :comments, :posts, on_delete: :cascade
+  end
+end
