a639b354f243f91a462feaa8910ef4145544d341
brytebee <brytebee@gmail.com>
2022-05-10 22:47:29 +0100

    :white_check_mark: Update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,22 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-  has_many :posts, foreign_key: 'author_id'
-  has_many :likes, foreign_key: 'author_id'
-  has_many :comments, foreign_key: 'author_id'
+         :recoverable, :rememberable, :validatable,
+         :lockable, :timeoutable
+         # :confirmable, 
+
+  has_many :posts, foreign_key: 'author_id', dependent: destroy_all
+  has_many :likes, foreign_key: 'author_id', dependent: destroy_all
+  has_many :comments, foreign_key: 'author_id', dependent: destroy_all
 
   validates :name, presence: true
   validates :posts_counter, numericality: { only_integer: true }
 
   def recent_posts
     posts.includes(:comments, :likes).order(created_at: :desc).limit(3)
+  end
+
+  def admin?
+    role == 'admin' ? true : false
   end
 end
