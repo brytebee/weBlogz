79d6ceb7f03430d39a050539b96fb76a4e25d477
brytebee <brytebee@gmail.com>
2022-05-10 22:46:26 +0100

    :white_check_mark: Update like model

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,8 +1,8 @@
 class Like < ApplicationRecord
   after_save :update_likes_counter
 
-  belongs_to :author, class_name: 'User'
-  belongs_to :post
+  belongs_to :author, class_name: 'User', dependent: destroy
+  belongs_to :post, dependent: destroy
 
   def update_likes_counter
     post.increment!(:likes_counter)
