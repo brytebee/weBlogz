8b93a98218a14de3869fcd27da90b92aa6216dda
brytebee <brytebee@gmail.com>
2022-05-11 13:42:57 +0100

    :white_check_mark: Update routes.rb
    Added edit, destroy and update routes to posts and comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users, only: %i[index show destroy] do
-    resources :posts, only: %i[index show new create destroy] do
-      resources :comments, only: %i[new create]
+    resources :posts, only: %i[index show new create destroy update] do
+      resources :comments, only: %i[new create create destroy update]
       resources :likes, only: %i[new create]
     end
   end
