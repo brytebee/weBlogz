aed1b7ad603b1e7c295833ceb07fc060a1c5f76f
brytebee <brytebee@gmail.com>
2022-05-13 12:00:25 +0100

    :rotating_light: Fix linters

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,24 +2,10 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    # user ||= User.new # guest user (not logged in)
-    # if user.admin?
-    #   can :manage, :all
-    # else
-    #   can :manage, Post, author_id: user.id
-    #   can :manage, Comment, author_id: user.id
-    #   can :read, :all
-    # end
     user ||= User.new # guest user (not logged in)
     if user.role == 'admin'
       can :manage, :all
     else
-      can :update, Post do |post|
-        post.author_id == user.id
-      end
-      can :update, Comment do |comment|
-        comment.author_id == user.id
-      end
       can :destroy, Post do |post|
         post.author == user
       end
@@ -31,32 +17,7 @@ class Ability
       can :create, Like
       can :read, :all
     end
-
-    # Define abilities for the passed in user here. For example:
-    #
     #   user ||= User.new # guest user (not logged in)
-    #   if user.admin?
-    #     can :manage, :all
-    #   else
-    #     can :read, :all
-    #   end
-    #
-    # The first argument to `can` is the action you are giving the user
-    # permission to do.
-    # If you pass :manage it will apply to every action. Other common actions
-    # here are :read, :create, :update and :destroy.
-    #
-    # The second argument is the resource the user can perform the action on.
-    # If you pass :all it will apply to every resource. Otherwise pass a Ruby
-    # class of the resource.
-    #
-    # The third argument is an optional hash of conditions to further filter the
-    # objects.
-    # For example, here the user can only update published articles.
-    #
-    #   can :update, Article, :published => true
-    #
-    # See the wiki for details:
-    # https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
+    #   user.admin? ? can :manage, :all : can :read, :all
   end
 end
